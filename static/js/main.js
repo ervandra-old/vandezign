f870b0ba76fcee24afa92039fa7ebabb5c333dfc
ervandra <ervandra.halim@cakrastudio.com>
2014-05-28 11:05:26 +0700

    Revert "Add Static design"
    This reverts commit dee18b964a930d956c536e3e0e9d52342ff267ab.

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-/*
- * Main Scripts
- * http://www.vandezign.net/
- * Copyright 2014, Van D'ZIGN
-*/
-;jQvan = jQuery.noConflict();
-jQvan(document).ready(function($){
-	$(document).foundation();
-});
