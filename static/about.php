dee18b964a930d956c536e3e0e9d52342ff267ab
ervandra <ervandra.halim@cakrastudio.com>
2014-05-28 11:03:51 +0700

    Add Static design
    initial static design -van

---
+++ static/about.php
@@ -0,0 +1,293 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="author" content="Ervandra Halim">
+<!--<meta name="description" content="">-->
+<link rel="author" href="https://plus.google.com/u/0/110270446180309877178">
+<title>Title</title>
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/fontface.css" />
+<link rel="stylesheet" href="css/font-awesome.min.css" />
+<link rel="stylesheet" href="css/main.css" />
+<script src="js/vendor/modernizr.js"></script>
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script src="js/main.js"></script>
+</head>
+  
+<body>
+
+	<div id="canvas" class="off-canvas-wrap" data-offcanvas>
+		<div class="inner-wrap">		
+			<!-- Off Canvas Menu -->
+			<aside class="left-off-canvas-menu">
+				<!-- whatever you want goes here -->
+				<div class="secondary-menu">
+					<div class="logo">
+						<h1><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-spinner fa-spin fa-stack-1x fa-inverse"></i></span>Van D'ZIGN</h1>
+					</div>
+					<hr>
+					<ul>
+						<li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-home fa-stack-1x fa-inverse"></i></span>Home</a></li>
+					</ul>
+					<hr>
+					<ul>
+						<li class=""><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-user fa-stack-1x fa-inverse"></i></span>About Us</a></li>
+						<li class=""><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-wrench fa-stack-1x fa-inverse"></i></span>Services</a></li>
+						<li class=""><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-picture-o fa-stack-1x fa-inverse"></i></span>Portfolio</a></li>
+						<li class=""><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-phone fa-stack-1x fa-inverse"></i></span>Contact Us</a></li>
+					</ul>
+					<hr>
+					<div class="hire-us">
+					<a class="primary button small" href="#"><span class="fa fa-beer"></span>Hire us today</a>
+					</div>
+					<hr>
+				</div>
+			</aside>
+
+    		<!-- main content goes here -->
+			<div class="sticky">
+				<nav class="top-bar show-for-small">
+					<a class="left-off-canvas-toggle menu-icon ">
+						<span class="fa fa-bars"></span>
+						<span>Van D'ZIGN</span>
+					</a>  
+				</nav>
+				
+				<nav class="top-bar hide-for-small" data-topbar>
+					<ul class="title-area">
+						<li class="name"><h1><a href="#"><span class="fa fa-spinner"></span>Van D'ZIGN</a></h1></li>
+						<!--<li class="toggle-topbar menu-icon left-off-canvas-toggle"><a href="#">Menu</a></li>-->
+					</ul>
+					<section class="top-bar-section">
+					<!-- Right Nav Section -->
+						<ul class="right">
+							<li class="divider"></li>
+							<li class=""><a href="about.php">About Us</a></li>
+							<li class="divider"></li>
+							<li class="has-dropdown"><a href="services.php">Services</a>
+								<ul class="dropdown">
+									<li><a href="#"><span class="fa fa-picture-o"></span>Web Design</a></li>
+									<li><a href="#"><span class="fa fa-gears"></span>Web Development</a></li>
+								</ul>
+							</li>
+							<li class="divider"></li>
+							<!--<li class=""><a href="portfolio.php">Portfolio</a></li>
+							<li class="divider"></li>-->
+							<li class=""><a href="contact.php">Contact Us</a></li>
+							<li class="divider"></li>
+							<li class="has-form">
+								<a class="vbutton primary button" href="/docs">Hire Us</a>
+							</li>
+						</ul>
+					
+					</section>
+				</nav>
+			</div>
+			
+			<!--<div class="button primary vbutton left-off-canvas-toggle">Menu</div>-->
+			<!-- Homepage Hero -->
+			<div id="home-hero">
+				<div class="row">
+					<div class="small-12 columns text-center">
+						<h1>About Us</h1>
+						<p>A little secret about us</p>
+					</div>
+				</div>
+			</div>	
+			
+			<!-- Homepage Offer -->		
+			<div id="home-offer" class="available">
+				<div class="row">
+					<div class="small-12 medium-9 columns small-only-text-center">
+						<h1>We are currently available to craft your masterpiece!</h1>
+					</div>
+					<div class="small-12 medium-3 columns">
+						<a class="button primary expand small">Request Quotation</a>
+					</div>
+				</div>
+			</div>
+			
+			<div id="content" class="sections">
+				<div class="row">
+					<div class="small-12 medium-4 columns">
+						<div class="sidebar">
+							<div class="widget">
+								<h2>Our Services</h2>
+								<div class="service-list">
+									<ul class="clearfix">
+										<li><a href="#"><span class="fa-stack fa"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-photo fa-stack-1x fa-inverse"></i></span>Web Design</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="small-12 medium-8 columns">
+						<div id="maincontent">
+							<h2>History of Us</h2>
+							<h5>A little group of integrity peoples</h5>
+							<p>We are a group of creative peoples which gather our interest, experiences, and our knowledges to bring a website services.</p>
+							
+							<hr>
+							
+							<h2>Our Values</h2>
+							<h5>We craft, not develop nor creating</h5>
+							<p>We have a simple rule for each of our projects, that we insist that our results are arts, that means your websites are artworks. We craft your websites with passion and latest cutting edge technologies, that will result in an clean, stylish, and awesome websites.</p>
+							<hr>
+							<h2>Business Solution</h2>
+							<h5>We provide marketing support for your business</h5>
+							<p>With latest technologies, we want to bring your business to another level, that is with your masterpiece websites. We can provide a support to see your websites progress, especially their online traffic or how many your website viewed by guest everyday.</p>
+							<hr>
+							<h2>Our Prime Directive</h2>
+							<h5>Website must be looking gorgeous on every device</h5>
+							<p>We propose you to have a Responsive Design, so that your website can adapt to any different viewing environments. This way you can have a larger market segments for your customers.</p>
+							<p>You have known that nowadays, smartphones users are rising, tablets also increasing. Because of this, we need a websites that can be viewed properly or even completely fine for them.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="section-separator">
+				<div class="row">
+					<div class="small-12 columns"><hr/></div>
+				</div>
+			</div>
+			
+			<!-- Why Choose Us-->
+			<div id="choose-us" class="sections text-center">
+				<div class="row">
+					<div class="small-12 columns">
+						<h2>Why Choose Us <span class="fa fa-question-circle end"></span></h2>
+						<h4>We craft your masterpiece</h4>
+						
+						<div class="mastery text-center">
+							<ul class="small-block-grid-2 medium-block-grid-4">
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-users fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Fresh Team</h6>
+									<p>With a fresh mind, your websites will be full of creativeness.</p>
+								</li>
+								
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-comments-o fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Fast Response</h6>
+									<p>Going through single project, you are our highest priority.</p>
+								</li>
+								
+								
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-clock-o fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Rapid Development</h6>
+									<p>Build websites will never be this fast, you are the witnesses!</p>
+								</li>
+								
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-trophy fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Premium CMS</h6>
+									<p>Our websites bundled with premium CMS and extended supports.</p>
+								</li>
+								
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-arrows-alt fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Responsive Website</h6>
+									<p><span class="fa fa-desktop fa-2x"></span>, <span class="fa fa-laptop fa-2x"></span>, <span class="fa fa-tablet fa-2x"></span>, <span class="fa fa-mobile-phone fa-2x"></span>, leave the worries with us!</p>
+								</li>
+								
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-sitemap fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Solid Structure</h6>
+									<p>Backbone of powerful websites are their foundations.</p>
+								</li>
+								
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-smile-o fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Satisfactory</h6>
+									<p>Your satisfaction are our irreplaceable diamonds.</p>
+								</li>						
+								
+								<li>
+									<div class="icon"><span class="fa-stack fa-lg fa-2x"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-thumbs-o-up fa-stack-1x fa-inverse"></i></span></div>
+									<h6>Socialize</h6>
+									<p>Never shy to mention your site through social media.</p>
+								</li>
+								
+							</ul>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div id="subscribe" class="">
+				<div class="row">
+					<div class="small-12 medium-8 columns">
+						<h5>Never missing out any of our special offers.</h5>
+						<p>Subscribe to receive immediately our monthly newsletter, or special events</p>
+					</div>
+					<div class="small-12 medium-4 columns">
+						<form>
+							<div class="row collapse">
+								<div class="small-8 columns">
+									<input type="email" placeholder="subscribe@email.com">
+								</div>
+								<div class="small-4 columns">
+									<input type="submit" class="button expand small" value="Subscribe">
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			
+			<div id="van-footer" class="sections">
+				<div class="row">
+					<div class="small-12 medium-5 columns">
+						<div class="about-van text-center">
+							<h3>Van D'ZIGN</h3>
+							<p>We love simplicity, clean, and gorgeous design. We put more focus to craft the All-In-One websites for every of our customers.</p>
+							<a href="#" class="button primary vbutton small"><span class="fa fa-coffee"></span>Learn about us</a>
+						</div>
+					</div>
+					<div class="small-12 medium-7 columns">
+						<div class="row collapse">
+							<div class="medium-6 columns">
+								<div class="footer-links usefull text-center">
+									<h4><span class="fa fa-search fa-2x"></span>What's Inside</h4>
+									<ul>
+										<li><span class="fa fa-picture-o"></span><a href="#">Web Design</a></li>
+										<li><span class="fa fa-gears"></span><a href="#">Web Development</a></li>										
+									</ul>
+								</div>
+							</div>
+							<div class="medium-6 columns">
+								<div class="footer-links recent text-center">
+									<h4><span class="fa fa-cog fa-spin fa-2x"></span>Recent Works</h4>
+									<ul>
+										<li><span class="fa fa-link"></span><a href="#">Exoticon</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div id="footer">
+				<div class="row">
+					<div class="small-12 medium-6 columns">
+						<div class="copyright small-only-text-center">
+							Copyright &copy;2014. Van D'Zign Studio. All Rights Reserved.
+						</div>
+					</div>
+				</div>
+			</div>
+			
+  			<!-- close the off-canvas menu -->
+  			<a class="exit-off-canvas"></a>
+		</div>
+	</div>
+	
+</body>
+</html>
