d0b70ac0f30fc37363f864e0f0bfb2465b656d4a
ervandra <ervandra.halim@cakrastudio.com>
2014-05-28 11:07:05 +0700

    Re-Add Static Pages
    re-add files -van

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+/*
+ * Main Scripts
+ * http://www.vandezign.net/
+ * Copyright 2014, Van D'ZIGN
+*/
+;jQvan = jQuery.noConflict();
+jQvan(document).ready(function($){
+	$(document).foundation();
+});
